e0b26650edc81c2169963f8934308ad9212b3274
Levani <levan.bulatsashvili@gmail.com>
2025-01-21 13:41:30 +0400

    feat: add coffee-context

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import SideBar from "./layouts/Sidebar";
 import CreateCoffeePage from "./pages/CoffeePage";
 import IngredientsPage from "./pages/IngredientsPage";
+import CoffeeContextProvider from "./contexts/CoffeeContext";
 
 function App() {
   return (
-    <div>
-      <SideBar />
-      <Routes>
-        <Route path="/" element={<CreateCoffeePage />} />
-        <Route path="/ingredients" element={<IngredientsPage />} />
-      </Routes>
-    </div>
+    <CoffeeContextProvider>
+      <div>
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<CreateCoffeePage />} />
+          <Route path="/ingredients" element={<IngredientsPage />} />
+        </Routes>
+      </div>
+    </CoffeeContextProvider>
   );
 }
 
