d60a3827dae6b7a881bc36569a93ac397f5e40ff
katsitadze <katsimariam@gmail.com>
2025-01-21 20:47:29 +0400

    Your commit message

--- src/pages/CoffeePage.jsx
+++ src/pages/CoffeePage.jsx
@@ -1,9 +1,64 @@
+import { useContext, useEffect, useState } from "react";
 import PageContainer from "../layouts/PageContainer";
+import { useCoffeeContext } from "../contexts/CoffeeContext";
 
-function CoffeePage() {
+const  CoffeePage = () => {
+  const { coffees, addCoffee, editCoffee, deleteCoffee } = useCoffeeContext();//to recive data from contextApi
+  const [newCoffee, setNewCoffee]=useState('')// is empty when the component is first rendered
+   const {data, loading, error,fetchData}=useFetch(
+     "https://crudapi.co.uk/api/v1/coffees",
+     'GET'
+   )
+
+//fetch data from crudapi
+
+const fetchCoffee = async ()=> {
+  try {
+ setLoadin(true);
+ const apiKey = import.meta.env.VITE_CRUDAPI_API_KEY;
+ const response = await fetch()
+
+  }
+}
+
+
+
+
+ const handleAddCofee =()=>{
+  const coffee = {
+    _uuid: Date.now().toString(), //  unique identifier
+    title: newCoffee,
+
+  };
+  addCoffee(coffee);
+  setNewCoffee('')
+ };
+
+ const handleDelteCoffee = (coffeeId)=>{
+  deleteCoffee(coffeeId)// send coffeeId to coffeeContex and delete
+ };
+
+ const handleEditCoffee= (coffeeId,newTitle) =>{
+   const updateCoffee={
+    ...coffees.fint((coffee)=> coffeeId._uuid === coffeeId),
+    tiitle:newTitle,
+  }
+   editCoffee (updateCoffee)
+  };
+  
+  
   return (
     <PageContainer>
-      <div>CoffeePage</div>
+
+      <div>Coffee Page
+
+
+
+
+
+
+        
+      </div>
     </PageContainer>
   );
 }
