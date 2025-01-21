e8c9d9f99603e1835d498a0032bb9b0b8bd39842
katsitadze <katsimariam@gmail.com>
2025-01-21 21:23:13 +0400

    Updated CoffeePage.jsx with changes

--- src/pages/CoffeePage.jsx
+++ src/pages/CoffeePage.jsx
@@ -1,33 +1,35 @@
 import { useContext, useEffect, useState } from "react";
 import PageContainer from "../layouts/PageContainer";
 import { useCoffeeContext } from "../contexts/CoffeeContext";
+import useFetch from "../hooks/useFetch";
 
 const  CoffeePage = () => {
   const { coffees, addCoffee, editCoffee, deleteCoffee } = useCoffeeContext();//to recive data from contextApi
   const [newCoffee, setNewCoffee]=useState('')// is empty when the component is first rendered
    const {data, loading, error,fetchData}=useFetch(
      "https://crudapi.co.uk/api/v1/coffees",
-     'GET'
+     'GET',
+     [],
    )
 
-//fetch data from crudapi
 
-const fetchCoffee = async ()=> {
-  try {
- setLoadin(true);
- const apiKey = import.meta.env.VITE_CRUDAPI_API_KEY;
- const response = await fetch()
 
-  }
+
+
+ useEffect (() => {
+if (data&&data.length) {
+  addCoffee(data)
 }
-
+}, [data,addCoffee])
 
 
 
  const handleAddCofee =()=>{
+  if (!newCoffee) return ;//if input isempty,doesnot addcoffee
+  
   const coffee = {
-    _uuid: Date.now().toString(), //  unique identifier
-    title: newCoffee,
+    _uuid: Date.now() ,//  unique identifier
+     title: newCoffee,
 
   };
   addCoffee(coffee);
@@ -38,26 +40,25 @@ const fetchCoffee = async ()=> {
   deleteCoffee(coffeeId)// send coffeeId to coffeeContex and delete
  };
 
- const handleEditCoffee= (coffeeId,newTitle) =>{
-   const updateCoffee={
-    ...coffees.fint((coffee)=> coffeeId._uuid === coffeeId),
-    tiitle:newTitle,
-  }
-   editCoffee (updateCoffee)
+ const handleEditCoffee= (coffeeId) =>{
+   navigator(`coffee/manage?id=${coffeeId}`)
   };
-  
+  //error da damateba
   
   return (
     <PageContainer>
 
-      <div>Coffee Page
+      <div>
+      <h1>Coffee Page</h1>
+      <input
+        type="text"
+        value={newCoffee}
+        onChange={(e) => setNewCoffee(e.target.value)}
 
-
-
-
-
-
-        
+      />
+      <button onClick={handleAddCoffee}>Add Coffee</button>
+      <button onClick={() => {handleEditCoffee(coffee._uuid, "Updated Coffee Title")}}></button>
+      <button onClick={() => handleDeleteCoffee(coffee._uuid)}>Delete</button>
       </div>
     </PageContainer>
   );
